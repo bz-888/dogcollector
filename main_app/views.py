13cc5cf9bff87d73cd69ba031686e0c29f13d249
bz-888 <benjamin.zheng95@gmail.com>
2023-12-12 21:41:29 -0600

    add create, edit, delete - working on one-to-many rel/feeding tracker

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-dogs = [
-    {'name': 'Amira', 'breed': 'greyhuahua', 'description': 'princess dog', 'age': 7},
-    {'name': 'Kiki', 'breed': 'pomchi', 'description': 'sweet little goblin', 'age': 3},
-]
+from .models import Dog
 
 # Create your views here.
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'description', 'age']
+    
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs'
+
 
 def home(request):
     return render(request, 'home.html')
@@ -14,4 +25,17 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    # go to db and get all the dogs
+    # and store the dogs in a variable called dogs
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
+
+# dog_id comes from the path in the urls
+# the argument of the function must match the param name
+def dogs_detail(request, dog_id):
+    # find the cat with id (dog_id) from the database
+    dog = Dog.objects.get(id=dog_id)
+    
+    # 'dog' is the variable name in dogs/detail.html
+    # the value of that variable is dog
+    return render(request, 'dogs/detail.html', {'dog': dog})
